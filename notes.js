cffaa57aec6d742bf5a4b2760ca097ff1405d9a3
“alessandro <aless.cam.h@gmail.com>
2022-11-27 10:28:14 -0500

    11/27

---
+++ notes.js
@@ -0,0 +1,26 @@
+/* 
+Semantic Versioning
+- Industry standard
+- "package": "Major.Minor.Patch"
+- Major: makes changes that won't work with earlier versions
+- Minor: adds functionality in a backwards-compatible manner (does not break).
+- Patch: Bug fixes (does not break)
+
+~
+- You should want to automatically update to the latest patches for bug fixes and security updates (while hopefully not breaking your project)
+- To freeze write as "1.2.8"
+- To update write as "~1.2.8" => latest 1.2.x
+
+^
+- The caret character allows future updates of the Minor updates and patches
+- "^3.4.5" => lastest 3.x.x.
+
+
+
+
+
+
+
+
+
+*/
